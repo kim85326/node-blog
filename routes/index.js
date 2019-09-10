e73bcde71bf25f9a03a05b818ba78d1d532c26e4
elaine <kim85326@gmail.com>
2019-09-11 00:17:22 +0800

    加入顯示前台文章列表功能

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 var express = require("express");
+var striptags = require("striptags");
+var moment = require("moment");
+var firebaseAdminDb = require("../connections/firebase_admin");
 var router = express.Router();
 
+const categoriesRef = firebaseAdminDb.ref("categories");
+const articlesRef = firebaseAdminDb.ref("articles");
+
 router.get("/", function(req, res, next) {
-    res.render("index", { title: "Express" });
+    let categories;
+    categoriesRef
+        .once("value")
+        .then((snapshot) => {
+            categories = snapshot.val();
+            return articlesRef.orderByChild("updateTime").once("value");
+        })
+        .then((snapshot) => {
+            const articles = [];
+
+            snapshot.forEach((snapshotChild) => {
+                const article = snapshotChild.val();
+                if (article.status === "public") {
+                    article.id = snapshotChild.key;
+                    articles.push(article);
+                }
+            });
+            res.render("index", { articles, categories, striptags, moment });
+        });
 });
 
 router.get("/post", function(req, res, next) {
